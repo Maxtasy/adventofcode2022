612e2452ba3adeb79604b37a9e00f039ef6873ea
Maxtasy <maxtasy88@web.de>
2022-12-18 05:34:51 +0100

    Finish day 1

--- day01-2.js
+++ day01-2.js
@@ -7,26 +7,13 @@ fs.readFile('day01-input.txt', 'utf8', (error, data) => {
     return;
   }
 
-  let mostCalories = [0, 0, 0];
-
   const inventories = data.split('\n\n');
 
-  const sortedInventories = inventories
+  const totals = inventories
     .map((inventory) => {
       const totalCalories = inventory
         .split('\n')
         .reduce((total, line) => (total += parseInt(line, 10)), 0);
-
-      if (totalCalories > mostCalories[0]) {
-        mostCalories[0] = totalCalories;
-        console.log(mostCalories);
-      } else if (totalCalories > mostCalories[1]) {
-        mostCalories[1] = totalCalories;
-        console.log(mostCalories);
-      } else if (totalCalories > mostCalories[2]) {
-        mostCalories[2] = totalCalories;
-        console.log(mostCalories);
-      }
 
       if (isNaN(totalCalories)) return 0;
 
@@ -34,7 +21,5 @@ fs.readFile('day01-input.txt', 'utf8', (error, data) => {
     })
     .sort();
 
-  console.log(mostCalories);
-
-  console.log(mostCalories[0] + mostCalories[1] + mostCalories[2]);
+  console.log(totals.slice(totals.length - 3).reduce((total, num) => (total += num), 0));
 });
