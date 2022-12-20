9fb86454a290fe36cadca1359eb7688d1033cf63
Maxtasy <maxtasy88@web.de>
2022-12-20 15:36:52 +0100

    Finish day 6

---
+++ day06-1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const allUnique = (array) => {
+  const encounteredItems = [];
+
+  array.forEach((item) => {
+    if (!encounteredItems.includes(item)) {
+      encounteredItems.push(item);
+    }
+  });
+
+  return array.length === encounteredItems.length;
+};
+
+fs.readFile('day06-input.txt', 'utf8', (error, data) => {
+  if (error) {
+    console.error(error);
+
+    return;
+  }
+
+  const sequence = [];
+
+  let subroutine = 0;
+
+  const lines = data.split('\n');
+  const communicationArray = lines[0].split('');
+
+  for (let i = 0; i < communicationArray.length; i += 1) {
+    sequence.push(communicationArray[i]);
+
+    subroutine += 1;
+
+    if (sequence.length < 5) continue;
+
+    sequence.shift();
+
+    if (allUnique(sequence)) {
+      break;
+    }
+  }
+
+  console.log(subroutine);
+});
